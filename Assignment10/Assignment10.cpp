3c40285619171e46667789dd6df1be85f771cd13
Brandon <bmbellat@gmail.com>
2016-04-05 18:35:59 -0600

    started assignment 10

---
+++ Assignment10/Assignment10.cpp
@@ -0,0 +1,60 @@
+/*
+*   Brandon Bell
+*   Assignment10
+*   Recitation: Th 1030am
+*   Guogui Ding
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+#include "Graph.h"
+using namespace std;
+
+// The basic structure of this main function was taken from my assignments 5.
+int main(int argc, char *argv[]){
+    // instantiate Graph instance, and menu selection variables.
+    MovieTree blockbuster(argv[1]);
+    int option = 0;
+
+    // The main loop. runs until quit is selected.
+    while( option != 4 ){
+        // Print the main menu.
+        cout<<"======Main Menu====="<< endl
+            <<"1. Print vertices"<< endl
+            <<"2. Find districts"<< endl
+            <<"3. Find shortest path"<< endl
+            <<"4. Quit"<< endl;
+
+        // Grab the option and convert to int with stringstream.
+        string s;
+        // this get line prevents a stray \n from hanging around messing up
+        // latter getline calls.
+        getline( cin, s );
+        stringstream ss(s);
+        ss >> option;
+
+        // Processes option and apply the relevent method.
+        if( option == 1 ){
+            string word;
+            cout<<"Enter title:";
+            getline( cin, word );
+            blockbuster.findMovie(word);
+        }
+        else if( option == 2 ){
+            string word;
+            cout<<"Enter title:";
+            getline( cin, word );
+            blockbuster.rentMovie(word);
+        }
+        else if( option == 3 ){
+            blockbuster.printMovieInventory();
+        }
+        else if( option == 4 ){
+            cout << "Goodbye!" << endl;
+        }
+    }
+return 0;
+}
---
+++ Assignment10/Graph.cpp
@@ -0,0 +1,42 @@
+/*
+*   Brandon Bell
+*   Assignment10
+*   Recitation: Th 1030am
+*   Guogui Ding
+*/
+
+#include<vector>
+#include<iostream>
+#include<"Graph.h">
+
+Graph::Graph(char *file){
+}
+Graph::~Graph(){
+}
+
+
+void Graph::addEdge(std::string v1, std::string v2, int weight){
+}
+
+
+void Graph::addVertex(std::string name){
+}
+
+
+void Graph::displayEdges(){
+}
+
+
+void Graph::assignDistricts(){
+}
+
+
+void Graph::shortestPath(std::string startingCity, std::string endingCity){
+}
+
+
+//call this from within assignDistricts to label the districts.
+//This method should implement a breadth first traversal from the startingCity
+//and assign all cities encountered the distID value
+void Graph::BFTraversalLabel(std::string startingCity, int distID){
+}
